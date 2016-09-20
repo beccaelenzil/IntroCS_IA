b4ec0997ca7fe55e97118b17705d52194dcaeb78
isabelalexander <Isabel@Isabels-MacBook-Pro.local>
2016-09-20 13:51:55 -0700

    First Commit

---
+++ Lists and Strings.py
@@ -0,0 +1,25 @@
+# python 2
+#
+# Name:
+#
+# Homework 2, Problem 1a and 1b
+# slicing and indexing challenges: Lists and Strings
+#
+
+pi = [3,1,4,1,5,9]
+e = [2,7,1]
+
+# Example problem (problem 0):
+# Creating the list [2,5,9] from pi and e
+answer0 = [ e[0] ] + pi[-2:]
+print answer0
+
+# Problem 1:
+# Creating the list [7,1] from pi and e
+answer1 = e[1:]
+print answer1
+
+# Problem 2:
+# Use pi and/or e to create the list [9,1,1]
+answer2 = [1::]
+print answer2
