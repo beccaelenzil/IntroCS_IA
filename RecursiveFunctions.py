a7231e8a610a7d478ecdd5c189514b0dec9ca2cc
isabelalexander <Isabel@Isabels-MacBook-Pro.local>
2016-10-16 14:12:19 -0700

    Recursive Functions

--- RecursiveFunctions.py
+++ RecursiveFunctions.py
@@ -1,5 +1,6 @@
 #https://trinket.io/python/85ef3ed403
 
+
 def fib(n):
     """Function that returns the nth term of the Fibonnaci sequence. The Fibonnaci sequence
         is found by adding the previous two terms.
@@ -20,14 +21,47 @@ def fibIter(n):
     is found by adding the previous two terms.
     """
 
-    result = [0,1]
+    result = []
 
-    while n > 1:
-        result = n + result
-        n = n - 1
-        return result
+    for i in range(n+1):
+        if i == 0:
+            result.append(0)
+        elif i == 1:
+            result.append(1)
+        else:
+            result.append(result[i-1] + result[i-2])
 
+    return result[-1]
 
 print "fibIter(5) == 5 = ",fibIter(5)," : ", 5 == fibIter(5)
 print "fibIter(11) == 89 = ",fibIter(11)," : ", 89 == fibIter(11)
+
+
+def listReverse(L):
+    """Recursive function that takes in a list L as an argument and returns the list L in reverse order.
+    """
+
+    if len(L) == 1:
+        return L
+    else:
+        return listReverse(L[1:]) + [L[0]]
+
+def listReverseIter(L):
+    """Iterative function that takes in a list L as an argument and returns the list L in reverse order.
+    """
+    length = len(L)
+    reverselist = []
+
+    for i in L:
+        reverselist.append(L[length-1])
+        length = length - 1
+
+        if length == 0:
+            return reverselist
+
+
+# List Reverse Tests
+print "listReverse([1,2,3,4]) == [4,3,2,1] = ",listReverse([1,2,3,4])," : ",listReverse([1,2,3,4]) == [4,3,2,1]
+print "listReverseIter([1,2,3,4]) == [4,3,2,1] = ",listReverseIter([1,2,3,4])," : ",listReverseIter([1,2,3,4]) == [4,3,2,1]
+
 
