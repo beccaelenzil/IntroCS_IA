4e1bc99672d3ea175366b3dd5913028e82d3d472
isabelalexander <Isabel@Isabels-MacBook-Pro.local>
2016-11-02 09:35:39 -0700

    segregation model

--- SegregationModel.py
+++ SegregationModel.py
@@ -16,25 +16,55 @@ def createBoard(width, height):
         A += [createOneRow(width)]
     return A
 
-def populateBoard(A):
-    height = len(A)
-    width = len(A[0])
-    counterX = 0
-    counterO = 0
-    counterempty = 0
-    numberofXcells = (width)*(height)*(1/3)
-    numberofOcells = (width)*(height)*(1/3)
+
+def printBoard(A):
+    for row in A:
+        line = ''
+        for col in row:
+            line += str(col)
+        print line
+
+def populateBoard(width, height, percX, percO):
+
+    A = createBoard(width, height)
+    numberofXcells = int((width)*(height)*percX)
+    numberofOcells = int((width)*(height)*percO)
     numberofemptycells = (width)*(height) - numberofXcells - numberofOcells
+    pop = numberofXcells*['X'] + numberofOcells* ['O'] + numberofemptycells*[" "]
+    pop = random.sample(pop, len(pop))
+    i = 0
     for row in range(height):
         for col in range(width):
-            while counterX <= numberofXcells:
-                while counterO <= numberofOcells:
-                    while counterempty <= numberofemptycells:
-                        A[row][col] = random.choice['X', 'O', " "]
-                        if A[row][col] == 'X':
-                            counterX += 1
-                        elif A[row][col] == 'O':
-                            counterO += 1
-                        else:
-                            counterempty += 1
+            A[row][col] = pop[i]
+            i+=1
+    return A
 
+
+#A = populateBoard(5, 5, 0.4, 0.4)
+#printBoard(A)
+
+def countNeighbors(row, col, A):
+    counterX = 0
+    counterO = 0
+    if A[row][col] == 'X' :
+        for r in range(row-1, row+2):
+            for c in range(col-1, col+2):
+                if A[r][c] == 'X':
+                    counterX += 1
+        if A[row][col] == 'X':
+            counterX = counterX - 1
+        return counterX
+    if A[row][col] == 'O' :
+        for r in range(row-1, row+2):
+            for c in range(col-1, col+2):
+                if A[r][c] == 'O':
+                    counterO += 1
+        if A[row][col] == 'O':
+            counterO = counterO - 1
+        return counterO
+    if A[row][col] == ' ':
+        return ""
+
+A = populateBoard(5,5, 0.4, 0.4)
+printBoard(A)
+print countNeighbors(1,1,A)
